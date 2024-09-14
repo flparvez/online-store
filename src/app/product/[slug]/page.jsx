84149a5edce25f2fcce34f974843c977b8fc4fee
flparvez <flparvez23@gmail.com>
2024-09-15 00:51:59 +0600

    add video

--- src/app/product/[slug]/page.jsx
+++ src/app/product/[slug]/page.jsx
@@ -58,7 +58,7 @@ const ProductPage =({ params }) => {
             <div className="aspect-w-16 aspect-h-9">
               <iframe
                 className="w-full lg:h-80 md:h-96 h-56 rounded-md"
-                src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                src={product?.video}
                 title="Product Video"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
