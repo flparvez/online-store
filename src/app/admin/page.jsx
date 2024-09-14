a5068dac9a29c618d4100f29567a186a71d51b19
flparvez <flparvez23@gmail.com>
2024-09-15 00:11:51 +0600

    add ui admin

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,25 +1,118 @@
 "use client"
 /** @format */
-
-import PageTitle from "@/components/PageTitle";
-
-import { DollarSign, Users, CreditCard, Activity } from "lucide-react";
-
-
-
 import {  useDeleteProductMutation, useGetProductsQuery } from "@/store/services/prodcutApi";
-import AdminProductTile from "@/components/ProductListAdmin";
-import { Card, CardContent } from "@/components/ui/card";
+
+import React, { useState } from "react";
+import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
+import {
+  IconArrowLeft,
+  IconBrandTabler,
+  IconSettings,
+  IconUserBolt,
+} from "@tabler/icons-react";
 import Link from "next/link";
-import { useState } from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import AdminProductTile from "@/components/ProductListAdmin";
 import SideNavbar from "@/components/sideNavbar";
-import { useGetSingleUserQuery } from "@/store/services/UserApi";
+
+
 
 
 
 export default function Home() {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "#",
+      icon: (
+        <IconBrandTabler className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Profile",
+      href: "#",
+      icon: (
+        <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Settings",
+      href: "#",
+      icon: (
+        <IconSettings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Logout",
+      href: "#",
+      icon: (
+        <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+  ];
+  const [open, setOpen] = useState(false);
 
 
+
+
+
+
+  // console.log(data?.products)
+  return (
+    
+       
+        
+        <div
+        className={cn(
+          "rounded-md flex  bg-gray-100 dark:bg-neutral-800 h-full  w-full mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+      
+        )}
+      >
+        <SideNavbar />
+        <Dashboard />
+ 
+        
+      </div>
+
+    
+  
+  );
+}
+
+
+
+export const Logo = () => {
+  return (
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+    >
+      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium text-black dark:text-white whitespace-pre"
+      >
+        Acet Labs
+      </motion.span>
+    </Link>
+  );
+};
+export const LogoIcon = () => {
+  return (
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+    >
+      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+    </Link>
+  );
+};
+ 
+// Dummy dashboard component with content
+const Dashboard = () => {
 
   const {data,isLoading} = useGetProductsQuery()
   const [deleteProduct] = useDeleteProductMutation();
@@ -27,25 +120,23 @@ export default function Home() {
 const handleDelete = async (productSlug) => {
     await deleteProduct(productSlug);
   };
-
-  // console.log(data?.products)
   return (
-    <div className=""> 
-    <SideNavbar />
-    <div className="flex flex-col gap-5  w-full">
-
-
-      <section className="">
-     
-        <CardContent className="w-full">
-        <div className="mb-5 w-full flex justify-end">
-        <h2> <Link href="/admin/product/add" >Add New Product</Link>
-          
-        </h2>
-      </div>
+    <div className="flex flex-1">
+      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+        <div className="flex gap-2">
+        <div  className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
+            >
+              <h2>Total Products</h2>
+            </div>
+      <div  className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse"
+            >
+              <h2>Total Order</h2>
+            </div>
+ 
+        </div>
         <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4">
         {data && data.products.length > 0
-          ? data.products.map((productItem)=> (
+          ? data?.products.map((productItem)=> (
               // eslint-disable-next-line react/jsx-key
               <AdminProductTile 
              
@@ -57,14 +148,7 @@ const handleDelete = async (productSlug) => {
             ))
           : null}
       </div>
-{/* <ProductList data={data && data?.products} /> */}
-
-        
-        </CardContent>
-
-
-      </section>
-    </div>
+      </div>
     </div>
   );
-}
+};
