e7f7e731b002ad05f3937894394688bbbfc0aaf8
flparvez <flparvez23@gmail.com>
2024-09-16 22:27:08 +0600

    edit middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,19 +6,12 @@ export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname
 
   const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail'
-  const isAdmin = path === '/admin'
-  const loginuser = path === '/auth/login' || path === '/auth/register' 
+
   const token = request.cookies.get('token')?.value || ''
 
   if(isPublicPath && token) {
     return NextResponse.redirect(new URL('/', request.nextUrl))
   } 
-   if(isAdmin && !token) {
-    return NextResponse.redirect(new URL('/not-admin', request.nextUrl))
-  }
-  if(loginuser && token) {
-    return NextResponse.redirect(new URL('/auth/profile', request.nextUrl))
-  }
 
   if (!isPublicPath && !token) {
     return NextResponse.redirect(new URL('/auth/login', request.nextUrl))
