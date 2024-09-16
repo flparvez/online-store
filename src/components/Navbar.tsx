393cbcc1232f7403953904bdb16763db799bac8e
flparvez <flparvez23@gmail.com>
2024-09-16 18:23:54 +0600

    Text Size Reduce

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
           {/* Logo */}
           <div className="flex-shrink-0">
             <Link href="/">
-              <h2 className="text-xl font-bold text-gray-800">Unique Store</h2>
+              <h2 className="text-sm font-bold text-gray-800">Unique Store</h2>
             </Link>
           </div>
 
